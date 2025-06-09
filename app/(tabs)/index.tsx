4f41a6f0d21194d22288c3290f4f3287b7826ad8
Subham Singha <subhamsingha@Subhams-MacBook-Air.local>
2025-06-09 18:38:06 +0530

    web setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,193 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import {
+  selectIsConnectedToRoom,
+  selectPeers,
+  useAVToggle,
+  useHMSActions,
+  useHMSStore,
+  useVideo,
+} from "@100mslive/react-sdk";
+import { CameraType, CameraView } from "expo-camera";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import "./styles.css";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+function JoinForm() {
+  const hmsActions = useHMSActions();
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    token: "",
+  });
+
+  const handleInputChange = (e) => {
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name = "", token = "" } = inputValues;
+    console.log("Joining room", name, token);
+
+    // use room code to fetch auth token
+    const authToken = await hmsActions.getAuthTokenByRoomCode({
+      roomCode: token,
+    });
+
+    try {
+      await hmsActions.join({ userName: name, authToken });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Join Room</h2>
+      <div className="input-container">
+        <input
+          required
+          value={inputValues.name}
+          onChange={handleInputChange}
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Your name"
+        />
+      </div>
+      <div className="input-container">
+        <input
+          id="token"
+          type="text"
+          name="token"
+          placeholder="Room code"
+          value={inputValues.token}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button className="btn-primary">Join</button>
+    </form>
+  );
+}
+
+function Conference() {
+  const peers = useHMSStore(selectPeers);
+  return (
+    <div className="conference-section">
+      <h2>Conference</h2>
+
+      <div className="peers-container">
+        {peers.map((peer) => (
+          <Peer key={peer.id} peer={peer} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Peer({ peer }) {
+  const { videoRef } = useVideo({
+    trackId: peer.videoTrack,
+  });
+  return (
+    <div className="peer-container">
+      <video
+        ref={videoRef}
+        className={`peer-video ${peer.isLocal ? "local" : ""}`}
+        autoPlay
+        muted
+        playsInline
+      />
+      <div className="peer-name">
+        {peer.name} {peer.isLocal ? "(You)" : ""}
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const { isLocalAudioEnabled, isLocalVideoEnabled, toggleAudio, toggleVideo } =
+    useAVToggle();
+  return (
+    <div className="control-bar">
+      <button className="btn-control" onClick={toggleAudio}>
+        {isLocalAudioEnabled ? "Mute" : "Unmute"}
+      </button>
+      <button className="btn-control" onClick={toggleVideo}>
+        {isLocalVideoEnabled ? "Hide" : "Unhide"}
+      </button>
+    </div>
+  );
+}
 
 export default function HomeScreen() {
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const hmsActions = useHMSActions();
+  const [facing, setFacing] = useState<CameraType>("back");
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  useEffect(() => {
+    window.onunload = () => {
+      if (isConnected) {
+        hmsActions.leave();
+      }
+    };
+  }, [hmsActions, isConnected]);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <div className="App">
+      {isConnected ? (
+        <>
+          <Conference />
+          <Footer />
+          <CameraView style={styles.camera} facing={facing}>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={toggleCameraFacing}
+              >
+                <Text style={styles.text}>Flip Camera</Text>
+              </TouchableOpacity>
+            </View>
+          </CameraView>
+        </>
+      ) : (
+        <JoinForm />
+      )}
+    </div>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    justifyContent: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { HMSRoomProvider } from "@100mslive/react-sdk";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
@@ -17,8 +23,24 @@ export default function RootLayout() {
     return null;
   }
 
+  if (Platform.OS === "web") {
+    return (
+      <HMSRoomProvider>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </HMSRoomProvider>
+    );
+  }
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
